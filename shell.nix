eb21d8604f9db9dd02ebb40b69341776438b6199
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-02-25 16:23:08 -0500

    shell nix

---
+++ shell.nix
@@ -0,0 +1,24 @@
+let
+pkgs = import <nixpkgs> { };
+dotfiles = builtins.fetchGit {
+    url = "https://github.com/WGUNDERWOOD/dotfiles";
+    rev = "2b1457479d611d6615dcba37803a85cbd35e8dad";
+};
+programs = "${dotfiles}/programs/";
+latexindent-fast = pkgs.callPackage "${programs}/latexindent-fast.nix" {};
+long-lines = pkgs.callPackage "${programs}/long-lines/long-lines.nix" {};
+spell-check = pkgs.callPackage "${programs}/spell-check/spell-check.nix" {};
+tex-check = pkgs.callPackage "${programs}/tex-check/tex-check.nix" {};
+tex-clean = pkgs.callPackage "${programs}/tex-clean.nix" {};
+todo-finder = pkgs.callPackage "${programs}/todo-finder/todo-finder.nix" {};
+in pkgs.mkShell {
+    buildInputs = [
+        pkgs.texlive.combined.scheme-full
+        latexindent-fast
+        long-lines
+        spell-check
+        tex-check
+        tex-clean
+        todo-finder
+    ];
+}
