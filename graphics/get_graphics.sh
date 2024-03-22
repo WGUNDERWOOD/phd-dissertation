5e0b0eeb75910256ba2cd5cd2169a49672d294e6
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-03-22 12:42:22 -0400

    mon

--- graphics/get_graphics.sh
+++ graphics/get_graphics.sh
@@ -5,9 +5,9 @@ cp ~/github/MondrianForests.jl/replication/partition_plots/plot_mondrian_process
 cp ~/github/MondrianForests.jl/replication/weather/weather_data.png .
 cp ~/github/MondrianForests.jl/replication/weather/weather_data_filled_partition.png .
 cp ~/github/MondrianForests.jl/replication/weather/weather_forest_2.png .
-cp ~/github/MondrianForests.jl/replication/weather/weather_forest_30.png .
 cp ~/github/MondrianForests.jl/replication/weather/weather_gcv.png .
 cp ~/github/MondrianForests.jl/replication/weather/weather_forest_design.png .
+cp ~/github/MondrianForests.jl/replication/weather/weather_debiased_forest_design.png .
 cp ~/github/research-martingale-yurinskii/julia/sim_*.pdf .
 
 pngquant --quality=30 --ext=.png --force *.png
