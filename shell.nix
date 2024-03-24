32224d3361884ed5cf34b2a9351f13c7ebdeb46e
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-03-24 17:06:40 -0400

    remove some refs

--- shell.nix
+++ shell.nix
@@ -1,9 +1,11 @@
 let
 pkgs = import <nixpkgs> { };
+
 dotfiles = builtins.fetchGit {
     url = "https://github.com/WGUNDERWOOD/dotfiles";
     rev = "8a0beb5f7dc88a9910eae652b24ae3b31d823c1a";
 };
+
 programs = "${dotfiles}/programs/";
 compress-pdf = pkgs.callPackage "${programs}/compress-pdf/compress-pdf.nix" {};
 long-lines = pkgs.callPackage "${programs}/long-lines/long-lines.nix" {};
@@ -13,6 +15,7 @@ tex-check = pkgs.callPackage "${programs}/tex-check/tex-check.nix" {};
 tex-clean = pkgs.callPackage "${programs}/tex-clean/tex-clean.nix" {};
 tex-fmt = pkgs.callPackage "${programs}/tex-fmt/tex-fmt.nix" {};
 todo-finder = pkgs.callPackage "${programs}/todo-finder/todo-finder.nix" {};
+
 in pkgs.mkShell {
     buildInputs = [
         pkgs.just
